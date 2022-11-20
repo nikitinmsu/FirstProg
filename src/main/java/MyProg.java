ce703831077502a4454daf328ad8890658ca2edd
Alexey Nikitin <vtb4056341@region.vtb.ru>
2022-11-20 11:14:39 +0300

    Init

---
+++ src/main/java/MyProg.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class MyProg {
+    public static void main(String[] args) {
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длинна текста" + text.length());
+    }
+}
